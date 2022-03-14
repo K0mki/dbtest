c8f59c7c3b1ed25988c1d7e475b6a7735c0e95cf
Igor Jeremic <igor@digitalcube.rs>
2022-03-14 09:16:31 +0100

    UI/UX change for taking params

--- konverzija.py
+++ konverzija.py
@@ -1,5 +1,17 @@
+import sys
+
 cetrespetadecimal_tablica = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A' , 'B', 'C', 'D', 'E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i']
-decimal = int(input("Unesite broj: "))
+
+if len(sys.argv)<2:
+    print(f'use {sys.argv[0]} number')
+    sys.exit()
+
+try:
+    decimal = int(sys.argv[1])
+except:
+    print(f'invalid parameter, use integer')
+    sys.exit()
+    
 cetrespetadecimal = ''
 while(decimal>0):
     ostatak = decimal%45
