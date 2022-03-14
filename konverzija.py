9b43af670bebc474d099068dce1b981cfb04f2c5
Stefan Kotarac <stefankotarac442@gmail.com>
2022-03-14 09:17:42 +0100

    change message

--- konverzija.py
+++ konverzija.py
@@ -9,7 +9,7 @@ if len(sys.argv)<2:
 try:
     decimal = int(sys.argv[1])
 except:
-    print(f'invalid parameter, use integer')
+    print(f'invalid parameter, please use integer')
     sys.exit()
     
 cetrespetadecimal = ''
