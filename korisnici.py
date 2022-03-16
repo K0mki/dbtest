3bb2123002d4029a64a211f606a25fd21075a0f6
Stefan <86250949+K0mki@users.noreply.github.com>
2022-03-16 15:37:02 +0100

    Add files via upload

---
+++ korisnici.py
@@ -0,0 +1,25 @@
+import psycopg2
+from config import config
+
+def connect():
+    connection = None
+    try:
+        params = config()
+        print("Connecting to database....")
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+        print('PostgreSQL database version: ')
+        cursor.execute('SELECT version()')
+        db_version = cursor.fetchone()
+        print (db_version)
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:  
+            connection.close()
+            print('Kraj')   
+
+if __name__  == "__main__":
+    connect()
